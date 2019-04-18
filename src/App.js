a4687538275785c3a88d2085d23be297491ecc85
Julien Duhem <dhmjulien@gmail.com>
2019-04-18 21:50:16 +0200

    First try of an ugly page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Page1 from './Components/Page1/Page1';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Page1 />
       </div>
     );
   }
---
+++ src/Components/Page1/Page1.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import wordart from '../../assets/images/wordart.png';
+import wordart2 from '../../assets/images/wordart2.png';
+import './Page1.css';
+
+class Page1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      explode: false,
+      appearTitle1: false,
+      appearTitle2: false,
+      clicked: false,
+      win: false
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(this.appearTitle1, 50);
+    setTimeout(this.explode, 5000);
+    setTimeout(this.unexplode, 5700);
+  }
+
+  appearTitle1 = () => {
+    this.setState({ appearTitle1: true });
+  }
+
+  explode = () => {
+    this.setState({ explode: true });
+  }
+
+  unexplode = () => {
+    this.setState({ explode: false });
+  }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked, appearTitle1: false, appearTitle2: true });
+  }
+
+  clickedBallon = () => {
+    this.setState({ win: true });
+  }
+
+  render() {
+    return (
+      <div className={'Page1' + (this.state.win ? ' win' : '')}>
+        <img src={wordart} className={'wordart' + (this.state.appearTitle1 ? ' appear' : '')} />
+        <img src={wordart2} className={'wordart2' + (this.state.appearTitle2 ? ' appear' : '')} />
+        <div>
+          <div className='issou'>
+            <button className='ballon' onClick={this.clickedBallon}>x</button>
+          </div>
+          <button className='button' onClick={this.handleClick}>Click me !</button>
+        </div>
+        <div className={'fail-container' + (this.state.clicked ? ' activation' : '')}>
+          <div className='fail'></div>
+        </div>
+        <div className='snoopdog'></div>
+        <div className='clippy'></div>
+        <div className='fire'></div>
+        <div className='obama'></div>
+        <div className='cigarettes'></div>
+        <div className={this.state.explode ? 'explosion' : ''}></div>
+      </div>
+    );
+  }
+}
+
+export default Page1;
