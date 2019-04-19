51fdb0a3fb89459ad3cd7ad746b1396fb4091b8a
Julien Duhem <dhmjulien@gmail.com>
2019-04-19 10:14:41 +0200

    Fixed links and css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Page1 from './Components/Page1/Page1';
 import Page2 from './Components/Page2/Page2';
 import Page4 from './Components/Page4/Page4';
+import P404 from './Components/P404/P404';
 import Voldemort from './Components/Voldemort/Voldemort';
 import './App.css';
 
@@ -14,6 +15,7 @@ class App extends Component {
         <Route path="/" exact component={Page1} />
         <Route path="/page2" component={Page2} />
         <Route path="/page4" component={Page4} />
+        <Route path="/404" component={P404} />
         <Route path="/voldemort" component={Voldemort} />
       </Router>
     );
